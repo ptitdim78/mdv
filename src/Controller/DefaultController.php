64a4f49e5ad9d38556dff989126b4ef16811c834
Joubert Tristan <t.joubert78980@gmail.com>
2021-05-31 17:22:33 +0200

    - Mise au point 	- Ajout des images dans l'index 		- mise au point reponsive 	- ajout d'un controller pour la page "à propos" 		- découpe de la page a propos 			- "À propos" 				- ajout titre 				- ajout texte 				- ajout image 				- stylisation titre 				- stylisation image 				- reponsive 	- ajout d'une rout vers l'acceuil 	- ajout d'un controller de la page mentions légalesdans le footer 		-"Mentions légales" 			- Ajout du titre 			- Ajout du sous-titre 			- ajout du texte de la page 			- stylisation sous-titre

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,26 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route ("/a_propos", name="a_propos")
+     */
+    public function aPropos(): Response
+    {
+        return $this->render('default/a_propos.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route ("/mentions_legales", name="mentions_legales")
+     */
+    public function contact(): Response
+    {
+        return $this->render('default/mentions_legales.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }

7d961ce0abf7f6c06d54bb1bd84addcec570f85e
Joubert Tristan <t.joubert78980@gmail.com>
2021-06-01 17:31:46 +0200

    ajout d'un controller Form -> php bin/console make:controller FormController 	ajout du orm pack -> composer require symfony/orm-pack (sous requete BDD) 	ajour maker bundle -> composer require --dev symfony/maker-bundle 	création d'une entity -> php bin/console make:entity 		ajout de la migration -> permettant d'en faire une table -> php bin/console make:migration --> php bin/console doctrine:migrations:migrate 	verification de la gestion des formulaires ->  composer require symfony/form 	verification de la sécurité formulaire --> composer require symfony/security-csrf 	lien entre twig et formulaire -->  composer require symfony/twig-bridge 	validation des formulaires -> composer require symfony/validator 	- création de contact type permettant de construire et parametrer le formulaire 	ajout -> composer require symfony/http-foundation 	-> création de la vu du form 	-> changement twig.yeml --> utilisation de bootstrap 	-> modification des proprietes DB php bin/console doctrine:schema:update --force 	-> dd($form)

---
+++ src/Controller/ContactType.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Contact;
+use Symfony\Component\DomCrawler\Field\TextareaFormField;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Votre nom'
+            ])
+            ->add('mail', EmailType::class, [
+                'label' => 'Votre mail'
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Objet'
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message'
+            ])
+            ->add('cookies', CheckboxType::class, [
+                'label' => 'En soumettant ce formulaire, j\'accepte que les informations saisies dans ce formulaire soit utilisées, exploitées, traitées pour permettre de me recontacter, pour m\'envoyer la newsletter, dans le cadre de la relation commerciale qui découle de cette demande de contact.'
+            ])
+            ->add('phone', TextType::class,[
+                'label'=>'Téléphone'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}

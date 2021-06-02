3cc3ce2f48966c2ce7207d375c719821fd043970
Dimitri <dimitri.bourlier@gmail.com>
2021-06-02 17:28:05 +0200

    ne pas garder

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FormController extends AbstractController
@@ -13,13 +15,28 @@ class FormController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      * @param Request $request
+     * @return Response
+     * @throws TransportExceptionInterface
      */
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact)->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-//            dd($form);
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('cabcee739b-cb55d7@inbox.mailtrap.io')
+                ->subject('Demande de devis')
+                ->htmlTemplate('mail/ContactMail.html.twig')
+                ->context([
+                    'name'=> $contact->getName(),
+                    'phone'=> $contact->getPhone(),
+                    'subject'=> $contact->getSubject(),
+                    'message'=> $contact->getMessage()
+                ]);
+
+            $mailer->send($email);
+
             $this->addFlash('success', 'Test réussi');
             return $this->redirectToRoute('contact');
         }

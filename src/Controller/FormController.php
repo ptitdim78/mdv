3919e0a8b227b9ebcbc515655cee9c1e98e49cf5
Joubert Tristan <t.joubert78980@gmail.com>
2021-06-02 17:24:32 +0200

    - Page 		- ajout icone 		- stylisation titre 		- traduction 		- parametrage des mails --> instalation mailer composer require symfony/mailer 			-mailtrap.com -> parmetrage dans le .env 			- template mail dans mail/ContactMail.html.twig 			- ajout du css_inline composer require twig/cssinliner-extra 			- ajout path '%kernel.project_dir%/public/images': images dans twig.yml pour ajouter des images dans le corps de mails 		-stylisation du corps de mail 			- alignement img 			- importation font 			-Stylisation du titre --> letter-spacing 5px; nom uppercase 			-stylisation du corps objet en bold; telephone en bold 			- stylisation du corps --> ::first-letter en violet 			- ajout du footer 			- ajustement responsive

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FormController extends AbstractController
@@ -13,13 +17,29 @@ class FormController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      * @param Request $request
+     * @throws TransportExceptionInterface
      */
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact)->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-//            dd($form);
+            $email = (new TemplatedEmail())
+                ->from($contact->getMail())
+                ->to('cabcee739b-cb55d7@inbox.mailtrap.io')
+                ->subject('demande de devis')
+                ->htmlTemplate('mail/ContactMail.html.twig')
+                ->context([
+                    'firstname'=>$contact->getFirstname(),
+                    'lastname'=>$contact->getLastname(),
+                    'phone'=>$contact->getPhone(),
+                    'subject'=>$contact->getSubject(),
+                    'message'=>$contact->getMessage(),
+                ]);
+
+            $mailer->send($email);
+
+
             $this->addFlash('success', 'Test réussi');
             return $this->redirectToRoute('contact');
         }

1329894694b81d78bf3750d0ce36324a43ae6ff3
Dimitri <dimitri.bourlier@gmail.com>
2021-06-08 11:43:41 +0200

    entity products controller products

---
+++ src/Entity/Products.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductsRepository::class)
+ */
+class Products
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $longueur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $hauteur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $profondeur;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $poids;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $composition;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $online;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getOnline(): ?bool
+    {
+        return $this->online;
+    }
+
+    public function setOnline(bool $online): self
+    {
+        $this->online = $online;
+
+        return $this;
+    }
+
+    public function getLongueur(): ?string
+    {
+        return $this->longueur;
+    }
+
+    public function setLongueur(?string $longueur): self
+    {
+        $this->longueur = $longueur;
+
+        return $this;
+    }
+
+    public function getHauteur(): ?string
+    {
+        return $this->hauteur;
+    }
+
+    public function setHauteur(?string $hauteur): self
+    {
+        $this->hauteur = $hauteur;
+
+        return $this;
+    }
+
+    public function getProfondeur(): ?string
+    {
+        return $this->profondeur;
+    }
+
+    public function setProfondeur(?string $profondeur): self
+    {
+        $this->profondeur = $profondeur;
+
+        return $this;
+    }
+
+    public function getPoids(): ?string
+    {
+        return $this->poids;
+    }
+
+    public function setPoids(?string $poids): self
+    {
+        $this->poids = $poids;
+
+        return $this;
+    }
+
+    public function getComposition(): ?string
+    {
+        return $this->composition;
+    }
+
+    public function setComposition(?string $composition): self
+    {
+        $this->composition = $composition;
+
+        return $this;
+    }
+}

dc2e2d34f059d00312a7de7434717f50ca72c1c9
Joubert Tristan <t.joubert78980@gmail.com>
2021-06-09 17:25:32 +0200

    - Ajout du bundle admin 		-creation du dashbord admin 		-> modification de la route ADMIN -> gestion_admin 		-> ajout des CRUDControllers 		-> ajout de la sécurité admin 		-> ajout du VichUploaderBundle 		-> + ajout formView dans admin

--- src/Controller/Admin/ProductsCrudController.php
+++ src/Controller/Admin/ProductsCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use Doctrine\DBAL\Types\BooleanType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProductsCrudController extends AbstractCrudController
 {
@@ -12,14 +19,21 @@ class ProductsCrudController extends AbstractCrudController
         return Products::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextareaField::new('description')->hideOnIndex(),
+            TextField::new('imageFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
+            ImageField::new('image')->setBasePath('uploads/image/')->onlyOnIndex(),
+            TextField::new('hauteur')->hideOnIndex(),
+            TextField::new('longueur')->hideOnIndex(),
+            TextField::new('profondeur')->hideOnIndex(),
+            TextField::new('poids')->hideOnIndex(),
+            TextareaField::new('composition')->hideOnIndex(),
+            BooleanField::new('online'),
         ];
     }
-    */
+
 }
--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -4,11 +4,15 @@ namespace App\Entity;
 
 use App\Repository\ProductsRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=ProductsRepository::class)
+ * @Vich\Uploadable
  */
 class Products
 {
@@ -36,12 +40,7 @@ class Products
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
-     * @param UploadedFile $image
-     * @Assert\File(
-     *     mimeTypes={"image/png", "image/svg+xml", "image/jpg", "image/jpeg"},
-     *     mimeTypesMessage="Le type de fichier et incorrect {{ type }}, vous devez choisir un fichier de type {{ types }}",
-     *     maxSize="2M", maxSizeMessage="Le fichier ne peut pas dépasser 2Mo"
-     * )
+     * @var string
      */
     private $image;
 
@@ -70,6 +69,17 @@ class Products
      */
     private $composition;
 
+    /**
+     * @ORM\Column(type="datetime", nullable= true)
+     * @var \DateTime
+     */
+    private $updateAt;
+
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
 
 
 
@@ -184,6 +194,54 @@ class Products
         $this->composition = $composition;
 
         return $this;
+    }
+
+//    public function getImageFile(): File
+//    {
+//        return $this->imageFile;
+//    }
+//
+//    public function setImageFile(File $image = null)
+//    {
+//        $this->imageFile = $image;
+//
+//        // VERY IMPORTANT:
+//        // It is required that at least one field changes if you are using Doctrine,
+//        // otherwise the event listeners won't be called and the file is lost
+//        if ($image) {
+//            // if 'updatedAt' is not defined in your entity, use another property
+//            $this->updateAt = new \DateTime('now');
+//        }
+//    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+        public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updateAt = new \DateTime('now');
+        }
     }
 
 }
